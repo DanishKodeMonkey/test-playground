795a7b583ddba21c88dd57f554508bf147916f98
danishkodemonkey <humberjini@hotmail.com>
2024-03-19 10:59:49 +0100

    Add caesar cipher tests, clean up dir

--- odin-calculator-test/odin-calculator.js
+++ odin-calculator-test/odin-calculator.js
@@ -1,17 +1,19 @@
-function add(num1, num2) {
-	return num1 + num2
+const calculator = {
+	add: function (num1, num2) {
+		return num1 + num2
+	},
+
+	subtract: function (num1, num2) {
+		return num1 - num2
+	},
+
+	divide: function (num1, num2) {
+		return num1 / num2
+	},
+
+	multiply: function (num1, num2) {
+		return num1 * num2
+	},
 }
 
-function subtract(num1, num2) {
-	return num1 - num2
-}
-
-function divide(num1, num2) {
-	return num1 / num2
-}
-
-function multiply(num1, num2) {
-	return num1 * num2
-}
-
-export { add, subtract, divide, multiply }
+export { calculator }
--- odin-calculator-test/odin-calculator.test.js
+++ odin-calculator-test/odin-calculator.test.js
@@ -1,17 +1,17 @@
-import { add, subtract, divide, multiply } from './odin-calculator'
+import { calculator } from './odin-calculator'
 
 test('add 2 + 2 = 4', () => {
-	expect(add(2, 2)).toBe(4)
+	expect(calculator.add(2, 2)).toBe(4)
 })
 
 test('subtract 3 - 1 = 2', () => {
-	expect(subtract(3, 1)).toBe(2)
+	expect(calculator.subtract(3, 1)).toBe(2)
 })
 
 test('divide 2 / 4 = 2', () => {
-	expect(divide(4, 2)).toBe(2)
+	expect(calculator.divide(4, 2)).toBe(2)
 })
 
 test('multiply 2 * 2 = 4', () => {
-	expect(multiply(2, 2)).toBe(4)
+	expect(calculator.multiply(2, 2)).toBe(4)
 })
